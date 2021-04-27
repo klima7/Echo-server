fc746f5042607697ac43ed646e8faf7c169233b8
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-04-27 21:11:20 +0200

    Server decomposed

--- main.cpp
+++ main.cpp
@@ -5,10 +5,56 @@
 #include <windows.h>
 
 #define DEFAULT_PORT 7
+#define MAX_CONNECTED_CLIENTS 3
 
 using namespace std;
 
 volatile bool running = true;
+int connected_clients = 0;
+CRITICAL_SECTION cs;
+
+// Prototypes
+BOOL WINAPI interrupt_handler(DWORD signal);
+void handle_client(sockaddr_in client_addr, SOCKET client_sock);
+void not_handle_client(sockaddr_in client_addr, SOCKET client_sock);
+void accept_client(SOCKET server_sock, sockaddr_in &client_addr, SOCKET &client_sock);
+SOCKET prepare_server(int port);
+void close_server(SOCKET server_sock);
+void setup_interrupt();
+int get_port(int argc, char *const *argv);
+
+struct client_data_t {
+    SOCKET client_sock;
+    sockaddr_in client_addr;
+};
+
+int main(int argc, char **argv) {
+
+    InitializeCriticalSection(&cs);
+
+    setup_interrupt();
+    int port = get_port(argc, argv);
+    SOCKET server_sock = prepare_server(port);
+
+    while(running) {
+        struct sockaddr_in client_addr;
+        SOCKET client_sock;
+
+        accept_client(server_sock, client_addr, client_sock);
+        handle_client(client_addr, client_sock);
+    }
+
+    close_server(server_sock);
+
+    DeleteCriticalSection(&cs);
+}
+
+void setup_interrupt() {
+    if (!SetConsoleCtrlHandler(interrupt_handler, TRUE)) {
+        cerr << "ERROR: Could not set control handler" << endl;
+        exit(EXIT_FAILURE);
+    }
+}
 
 // Handler invoked on ctrl-c
 BOOL WINAPI interrupt_handler(DWORD signal) {
@@ -19,108 +65,22 @@ BOOL WINAPI interrupt_handler(DWORD signal) {
     return TRUE;
 }
 
-int main(int argc, char **argv) {
-
+int get_port(int argc, char *const *argv) {
     int port = DEFAULT_PORT;
-
-    // Setting ctrl-c handler
-    if (!SetConsoleCtrlHandler(interrupt_handler, TRUE)) {
-        cerr << "ERROR: Could not set control handler" << endl;
-        return EXIT_FAILURE;
-    }
-
-    // Get port
     if(argc > 1) {
         port = atoi(argv[1]);
         if(port < 0 || port > 65535) {
             cerr << "Invalid port number supplied" << endl;
-            return 1;
+            exit(EXIT_FAILURE);
         }
     }
-    cout << "Server is listening on port " << port << endl;
+    return port;
+}
 
-    // WSAStartup
-    WSADATA wsadata;
-    int result = WSAStartup(MAKEWORD(2, 2), &wsadata);
-    if (result != 0) {
-        cerr << "Unable to initialize winsock. Error: " << WSAGetLastError() << endl;
-        return EXIT_FAILURE;
-    }
+void close_server(SOCKET server_sock) {// close socket
 
-    // Socket
-    SOCKET server_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_sock == INVALID_SOCKET) {
-        cerr << "Unable to create socket. Error: " << WSAGetLastError() << endl;
-        WSACleanup();
-        return EXIT_FAILURE;
-    }
-
-    // Bind
-    struct sockaddr_in sockaddr;
-    sockaddr.sin_family = AF_INET;
-    sockaddr.sin_port = htons(port);
-    sockaddr.sin_addr.s_addr = INADDR_ANY;
-    result = bind(server_sock, (struct sockaddr *) &sockaddr, sizeof(sockaddr_in));
-    if (result == SOCKET_ERROR) {
-        cerr << "Unable to bind. Error: " << WSAGetLastError << endl;
-        WSACleanup();
-        return EXIT_FAILURE;
-    }
-
-    // Listen
-    result = listen(server_sock, 10);
-    if(result == SOCKET_ERROR) {
-        cerr << "Unable to listen. Error: " << WSAGetLastError << endl;
-        WSACleanup();
-        return EXIT_FAILURE;
-    }
-
-    while(running) {
-
-        // Accept
-        cout << "Waiting to accept client" << endl;
-        struct sockaddr_in client_addr;
-        int addrlen;
-        SOCKET client_sock = accept(server_sock, (struct sockaddr *) &client_addr, &addrlen);
-        if (result == INVALID_SOCKET) {
-            cerr << "Unable to accept. Error: " << WSAGetLastError << endl;
-            WSACleanup();
-            return EXIT_FAILURE;
-        }
-
-        // Display info
-        char *client_ip = inet_ntoa(client_addr.sin_addr);
-        int client_port = client_addr.sin_port;
-        cout << "Client " << client_ip << " connected on port " << client_port << endl;
-
-        while (true) {
-
-            // Receive
-            char buffer[100];
-            int received_bytes = recv(client_sock, buffer, 100, 0);
-            if (received_bytes <= 0) break;
-
-            // Display info
-            string msg(buffer, received_bytes);
-            cout << "Client " << client_ip << ":" << client_port << " SAYS: " << msg;
-            cout << " (" << received_bytes << " bytes)" << endl;
-
-            // Send
-            result = send(client_sock, buffer, received_bytes, 0);
-            if (result == SOCKET_ERROR) break;
-
-            if(!running) break;
-        }
-
-        // Display info
-        cout << "Client " << client_ip << " on port " << client_port << " disconnected" << endl;
-
-        close(client_sock);
-        free(client_ip);
-    }
-
-    // close socket
-    result = closesocket(server_sock);
+    // Close server socket
+    int result = closesocket(server_sock);
     if(result == SOCKET_ERROR) {
         cerr << "Unable to close socket. Error: " << WSAGetLastError << endl;
         WSACleanup();
@@ -134,4 +94,129 @@ int main(int argc, char **argv) {
     cout << "Server closed" << endl;
     sleep(5);
     exit(EXIT_SUCCESS);
+}
+
+SOCKET prepare_server(int port) {
+
+    // WSAStartup
+    WSADATA wsadata;
+    int result = WSAStartup(MAKEWORD(2, 2), &wsadata);
+    if (result != 0) {
+        cerr << "Unable to initialize winsock. Error: " << WSAGetLastError() << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // Socket
+    SOCKET server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sock == INVALID_SOCKET) {
+        cerr << "Unable to create socket. Error: " << WSAGetLastError() << endl;
+        WSACleanup();
+        exit(EXIT_FAILURE);
+    }
+
+    // Bind
+    struct sockaddr_in sockaddr;
+    sockaddr.sin_family = AF_INET;
+    sockaddr.sin_port = htons(port);
+    sockaddr.sin_addr.s_addr = INADDR_ANY;
+    result = bind(server_sock, (struct sockaddr *) &sockaddr, sizeof(sockaddr_in));
+    if (result == SOCKET_ERROR) {
+        cerr << "Unable to bind. Error: " << WSAGetLastError << endl;
+        WSACleanup();
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen
+    result = listen(server_sock, 10);
+    if(result == SOCKET_ERROR) {
+        cerr << "Unable to listen. Error: " << WSAGetLastError << endl;
+        WSACleanup();
+        exit(EXIT_FAILURE);
+    }
+    cout << "Server is listening on port " << port << endl;
+
+    return server_sock;
+}
+
+void accept_client(SOCKET server_sock, sockaddr_in &client_addr, SOCKET &client_sock) {
+
+    // Accept
+    cout << "Waiting to accept client" << endl;
+    client_sock= accept(server_sock, (struct sockaddr *) &client_addr, NULL);
+    if (client_sock == INVALID_SOCKET) {
+        cerr << "Unable to accept. Error: " << WSAGetLastError << endl;
+        WSACleanup();
+        exit(EXIT_FAILURE);
+    }
+}
+
+DWORD WINAPI client_thread(LPVOID args) {
+    struct client_data_t *data = (struct client_data_t *)args;
+
+    EnterCriticalSection(&cs);
+
+    if(connected_clients >= MAX_CONNECTED_CLIENTS) {
+        LeaveCriticalSection(&cs);
+        not_handle_client(data->client_addr, data->client_sock);
+    }
+
+    else {
+        connected_clients++;
+        LeaveCriticalSection(&cs);
+        handle_client(data->client_addr, data->client_sock);
+
+        EnterCriticalSection(&cs);
+        connected_clients--;
+        LeaveCriticalSection(&cs);
+    }
+}
+
+void handle_client(sockaddr_in client_addr, SOCKET client_sock) {
+
+    // Display info
+    char *client_ip = inet_ntoa(client_addr.sin_addr);
+    int client_port = client_addr.sin_port;
+    cout << "Client " << client_ip << " connected on port " << client_port << endl;
+
+    while (true) {
+
+        // Receive
+        char buffer[100];
+        int received_bytes = recv(client_sock, buffer, 100, 0);
+        if (received_bytes <= 0) break;
+
+        // Display info
+        string msg(buffer, received_bytes);
+        cout << "Client " << client_ip << ":" << client_port << " SAYS: " << msg;
+        cout << " (" << received_bytes << " bytes)" << endl;
+
+        // Send
+        int result = send(client_sock, buffer, received_bytes, 0);
+        if (result == SOCKET_ERROR) break;
+
+        if(!running) break;
+    }
+
+    // Close client socket
+    close(client_sock);
+    free(client_ip);
+
+    // Display info
+    cout << "Client " << client_ip << " on port " << client_port << " disconnected" << endl;
+}
+
+void not_handle_client(sockaddr_in client_addr, SOCKET client_sock) {
+
+    // Display info
+    char *client_ip = inet_ntoa(client_addr.sin_addr);
+    int client_port = client_addr.sin_port;
+    cout << "Refused to handle client " << client_ip << " on port " << client_port << endl;
+
+    // Send error message
+    char message[] = "Unable to respond. Too many connections";
+    send(client_sock, message, strlen(message), 0);
+
+    // Close client socket
+    close(client_sock);
+    free(client_ip);
 }
